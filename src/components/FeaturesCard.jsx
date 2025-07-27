194f32659a575f59485cba2a96c5bdaedd83e8b5
Emanuele-Comand <emanuelecomand137@gmail.com>
2025-07-28 00:05:34 +0200

    Features carousel done

--- src/components/FeaturesCard.jsx
+++ src/components/FeaturesCard.jsx
@@ -6,7 +6,7 @@ const FeaturesCard = ({ card }) => {
     <>
       <div className="py-16 text-center">
         <div
-          className={`bg-white rounded-lg max-w-[368px] cursor-pointer ${
+          className={`bg-white rounded-xl max-w-[368px] cursor-pointer ${
             card.useBackground ? "bg-cover bg-center text-white" : ""
           }`}
         >
@@ -16,10 +16,16 @@ const FeaturesCard = ({ card }) => {
               card.useBackground
                 ? {
                     backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${card.image})`,
+                    backgroundSize:
+                      card.id === "security" ? "120% 120%" : "cover",
+                    backgroundPosition:
+                      card.id === "security" ? "center center" : "center",
                   }
                 : {}
             }
-            className="flex flex-col bg-gray-100 rounded-xl p-4 transition-shadow min-h-[572px] bg-cover bg-center"
+            className={`flex flex-col bg-white rounded-xl p-4 transition-shadow min-h-[572px] bg-cover bg-center ${
+              card.id === "security" ? "relative" : ""
+            }`}
           >
             <p
               className={`text-sm ${
@@ -38,10 +44,20 @@ const FeaturesCard = ({ card }) => {
             {!card.useBackground && (
               <img
                 src={card.image}
-                className={`w-full h-full object-cover rounded-lg py-16 ${
-                  card.id === "health" ? "w-3/4 h-40 mx-auto" : ""
-                }`}
+                className={`w-full h-full object-cover rounded-lg py-16 
+                  ${card.id === "health" ? "w-3/4 h-40 mx-auto" : ""}`}
               />
+            )}
+            {card.id === "security" && (
+              <div className="absolute inset-0 flex items-center justify-center">
+                <div className="w-40 h-40 border-5 border-red-500 rounded-full flex items-center justify-center">
+                  <div className="w-32 h-32 border-5 border-red-500 rounded-full flex items-center justify-center">
+                    <div className="bg-red-500 text-white rounded-full w-24 h-24 flex items-center justify-center font-bold text-lg">
+                      SOS
+                    </div>
+                  </div>
+                </div>
+              </div>
             )}
             <div className="flex items-end justify-end mt-auto">
               <FontAwesomeIcon icon={faCirclePlus} size="2x" />
--- src/components/FeaturesCarousel.jsx
+++ src/components/FeaturesCarousel.jsx
@@ -1,21 +1,29 @@
 import Container from "./Container";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Scrollbar } from "swiper/modules";
 import FeaturesCard from "./FeaturesCard";
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/navigation";
+import "swiper/css/scrollbar";
 
 const FeaturesCarousel = ({ featuresData }) => {
   return (
-    <>
-      <Swiper spaceBetween={160} slidesPerView={3}>
+    <div className="swiper-container">
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={160}
+        slidesPerView={3}
+        pagination={{
+          clickable: true,
+        }}
+      >
         {featuresData.map((card) => (
           <SwiperSlide key={card.id}>
             <FeaturesCard card={card} />
           </SwiperSlide>
         ))}
       </Swiper>
-    </>
+    </div>
   );
 };
 
