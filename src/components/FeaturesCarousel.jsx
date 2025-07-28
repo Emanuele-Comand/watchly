7afd49647bbba96caee2ec73a1c2f959e85ffc93
Emanuele-Comand <emanuelecomand137@gmail.com>
2025-07-28 04:43:00 +0200

    Minor changes to FeaturesCarousel

--- src/components/FeaturesCarousel.jsx
+++ src/components/FeaturesCarousel.jsx
@@ -1,20 +1,28 @@
 import Container from "./Container";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Scrollbar } from "swiper/modules";
+import { Pagination, Mousewheel } from "swiper/modules";
 import FeaturesCard from "./FeaturesCard";
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 const FeaturesCarousel = ({ featuresData }) => {
   return (
     <div className="swiper-container">
       <Swiper
-        modules={[Pagination]}
+        modules={[Pagination, Mousewheel]}
         spaceBetween={160}
         slidesPerView={3}
         pagination={{
           clickable: true,
+        }}
+        allowTouchMove={true}
+        simulateTouch={true}
+        touchStartPreventDefault={true}
+        touchMoveStopPropagation={true}
+        mousewheel={{
+          enabled: true,
+          forceToAxis: true,
+          sensitivity: 1,
         }}
       >
         {featuresData.map((card) => (
