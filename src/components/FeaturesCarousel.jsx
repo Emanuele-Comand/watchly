83c7e0ea49cc99714273dd2aafcad756e4b0671a
Emanuele-Comand <emanuelecomand137@gmail.com>
2025-08-17 00:07:54 +0200

    Working on FeaturesCarousel responsiveness

--- src/components/FeaturesCarousel.jsx
+++ src/components/FeaturesCarousel.jsx
@@ -6,7 +6,41 @@ const FeaturesCarousel = ({ featuresData, onCardClick }) => {
     <FeaturesCard card={card} onClick={() => onCardClick(card.id)} />
   );
 
-  return <Carousel data={featuresData} renderCard={renderFeaturesCard} />;
+  const responsiveConfig = {
+    modules: [Pagination, Mousewheel],
+    spaceBetween: 20,
+    slidesPerView: 1,
+    pagination: {
+      clickable: true,
+    },
+    allowTouchMove: true,
+    simulateTouch: true,
+    touchStartPreventDefault: true,
+    touchMoveStopPropagation: true,
+    mousewheel: {
+      enabled: true,
+      forceToAxis: true,
+      sensitivity: 1,
+    },
+    breakpoints: {
+      640: {
+        spaceBetween: 40,
+        slidesPerView: 2,
+      },
+      1024: {
+        spaceBetween: 160,
+        slidesPerView: 3,
+      },
+    },
+  };
+
+  return (
+    <Carousel
+      data={featuresData}
+      renderCard={renderFeaturesCard}
+      swiperConfig={responsiveConfig}
+    />
+  );
 };
 
 export default FeaturesCarousel;
